72f35db2ffc5e2833231e94f004cb63efab9173d
youneszh1 <younes.zh2016@gmail.com>
2024-12-20 22:57:12 +0100

    Candidat-User relationship. fix model (deletion of unused code)

--- src/main/java/com/example/edoctorat/models/UserModel.java
+++ src/main/java/com/example/edoctorat/models/UserModel.java
@@ -9,7 +9,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Entity
-//@Data
+@Data
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,92 +26,6 @@ public class UserModel {
     private LocalDateTime date_joined;
     private LocalDateTime last_login;
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getIs_superuser() {
-        return is_superuser;
-    }
-
-    public void setIs_superuser(String is_superuser) {
-        this.is_superuser = is_superuser;
-    }
-
-    public String getIs_staff() {
-        return is_staff;
-    }
-
-    public void setIs_staff(String is_staff) {
-        this.is_staff = is_staff;
-    }
-
-    public String getIs_active() {
-        return is_active;
-    }
-
-    public void setIs_active(String is_active) {
-        this.is_active = is_active;
-    }
-
-    public LocalDateTime getDate_joined() {
-        return date_joined;
-    }
-
-    public void setDate_joined(LocalDateTime date_joined) {
-        this.date_joined = date_joined;
-    }
-
-    public LocalDateTime getLast_login() {
-        return last_login;
-    }
-
-    public void setLast_login(LocalDateTime last_login) {
-        this.last_login = last_login;
-    }
 }
 
